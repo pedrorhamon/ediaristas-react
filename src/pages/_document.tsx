81dd14f763e0aacfdd2632c2df619ee22a1d116e
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-02-25 20:32:31 -0300

    problema de import

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,40 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import React from "react";
+import Document, {
+  Html, Head, Main, NextScript, DocumentContext, DocumentInitialProps
+} from "next/document";
+import {ServerStyleSheets} from '@mui/material/styles';
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+
+
+export default class MyDocument extends Document{
+
+  static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
+      const sheets = new ServerStyleSheets(),
+        originalRenderPage = ctx.renderPage;
+        ctx.renderPage = () => originalRenderPage({
+          enhanceApp: (App) => (props) => sheets.collect(<App {...props}/>)
+        })
+
+       const initialProps = Document.getInitialProps(ctx);
+
+       return {
+        ...initialProps,
+        styles: [
+          ...React.Children.toArray((await initialProps).styles),
+          sheets.getStyleElement(),
+        ],
+       }
+  }
+
+  render(): JSX.Element {
+      return <Html>
+        <Head>
+
+        </Head>
+        <body>
+          <Main/>
+          <NextScript/>
+        </body>
+      </Html>
+  }
 }
