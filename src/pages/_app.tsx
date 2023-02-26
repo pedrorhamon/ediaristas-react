8b3aaa5a3d58e1c8975a1a8d9fd0f5a7eb8aae73
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-02-26 20:00:27 -0300

    Atualizando dependency

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,11 +10,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>e-diaristas</title>
-        <link
-            rel="stylesheet"
-            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-        />
+  
       </Head>
       <Component {...pageProps} />
     </>
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import Document, {
   Html, Head, Main, NextScript, DocumentContext, DocumentInitialProps
 } from "next/document";
-import {ServerStyleSheets} from '@mui/material/styles';
+import {ServerStyleSheets} from '@mui/styles';
+
 
 
 
 export default class MyDocument extends Document{
+  
 
   static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
       const sheets = new ServerStyleSheets(),
@@ -29,9 +31,7 @@ export default class MyDocument extends Document{
   render(): JSX.Element {
       return <Html>
         <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com"/>
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;1,100;1,200;1,300&display=swap" rel="stylesheet"/>
+        
         </Head>
         <body>
           <Main/>
