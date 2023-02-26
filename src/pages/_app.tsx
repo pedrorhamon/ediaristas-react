b3a5fecffbb7b74dd9177b9094f4b814327a3f36
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-02-26 12:47:46 -0300

    removendo jss-server com document

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,12 @@
 import '@styles/globals.css'
 import type { AppProps } from 'next/app'
 import Head from 'next/head';
+import { useEffect } from 'react';
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    document.querySelector('#jss-server-side')?.remove();
+  }, [])
   return (
     <>
       <Head>
